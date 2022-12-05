f9a16d328f349828409507e0362ff6e40453bad5
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-12-05 16:54:52 +0300

    Coffee controller

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,2 +1,12 @@
 class CoffeesController < ApplicationController
+
+    def index
+        coffee = Coffee.all
+        render json: coffee
+   end
+
+   def show
+   coffee = Coffee.find_by!(id: params[:id])
+   render json: coffee
+   end
 end
