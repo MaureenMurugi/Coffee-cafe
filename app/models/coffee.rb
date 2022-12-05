f098dddf1294b385459ca79d0e61e4dfef946d45
Kelvin M Matheka <beingmatheka@gmail.com>
2022-12-05 11:56:09 +0300

    adding relations

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,2 +1,4 @@
 class Coffee < ApplicationRecord
+  has_many :users
+  has_many :reviews, through: :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :reviews, through: :coffees
+  has_many :coffees
 end
