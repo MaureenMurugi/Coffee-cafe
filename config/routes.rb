4e1a2f66761191afc0a919a607314d079defd83d
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-12-06 22:11:09 +0300

    Remodel coffee resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :logins
   resources :reviews, only: [:index, :create, :show, :destroy, :update]
   resources :coffees, only: [:index, :show]
-  resources :users, only: [:index, :create, :show, :destroy ]
+  resources :users, only: [:index, :create, :show, :destroy]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
